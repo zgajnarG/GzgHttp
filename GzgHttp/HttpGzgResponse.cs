204c8787f22f1ddbb4898ecd95056b59b943c240
Guillaume Zgajnar <guillaume.zgajnar@gonexa.fr>
2022-07-30 13:29:47 +0200

    - Renommage file ResponseStream - Ajout des header dans la réponse - Utilisation de la réponse gzg pour les méthodes basiques

--- GzgHttp/HttpGzgResponse.cs
+++ GzgHttp/HttpGzgResponse.cs
@@ -2,33 +2,36 @@
 
 public class HttpGzgResponse<T>
 {
-    public readonly bool isSuccess;
-    public readonly T responseContent;
-    public readonly string errorMessage;
-    public readonly int statusCode;
+    public readonly bool IsSuccess;
+    public readonly T ResponseContent;
+    public readonly string ErrorMessage;
+    public readonly int StatusCode;
+    public Dictionary<string , IEnumerable<string>> Headers;
+
 
     public HttpGzgResponse(bool isSuccess, T responseContent , string errorMessage , int statusCode )
     {
-        this.isSuccess = isSuccess;
-        this.responseContent = responseContent;
-        this.errorMessage = errorMessage;
-        this.statusCode = statusCode;
+        this.IsSuccess = isSuccess;
+        this.ResponseContent = responseContent;
+        this.ErrorMessage = errorMessage;
+        this.StatusCode = statusCode;
     }
 
     public HttpGzgResponse(bool isSuccess, T responseContent , int statusCode)
     {
-        this.isSuccess = isSuccess;
-        this.responseContent = responseContent;
-        this.errorMessage = String.Empty;
-        this.statusCode = statusCode;
+        this.IsSuccess = isSuccess;
+        this.ResponseContent = responseContent;
+        this.ErrorMessage = String.Empty;
+        this.StatusCode = statusCode;
     }
     public HttpGzgResponse(bool isSuccess, string errorMessage , int statusCode)
     {
-        this.isSuccess = isSuccess;
-        this.errorMessage = errorMessage;
-        this.responseContent = default;
-        this.statusCode = statusCode;
+        this.IsSuccess = isSuccess;
+        this.ErrorMessage = errorMessage;
+        this.ResponseContent = default;
+        this.StatusCode = statusCode;
     }
+
 
 
 }
---
+++ GzgHttp/HttpGzgResponseStream.cs
@@ -0,0 +1,33 @@
+﻿namespace GzgHttp;
+
+public class HttpGzgResponseStream : HttpGzgResponse<Stream> , IDisposable
+{
+
+    private bool dispose = false;
+
+    public HttpGzgResponseStream(bool isSuccess, Stream responseContent, string errorMessage , int statusCode) : base(isSuccess, responseContent, errorMessage, statusCode)
+    {
+    }
+
+    public HttpGzgResponseStream(bool isSuccess, Stream responseContent, int statusCode) : base(isSuccess, responseContent , statusCode)
+    {
+    }
+
+    public HttpGzgResponseStream(bool isSuccess, string errorMessage, int statusCode) : base(isSuccess, errorMessage , statusCode)
+    {
+    }
+
+
+    public void Dispose()
+    {
+        if (!dispose)
+        {
+            if(ResponseContent != null)
+            {
+                ResponseContent.Dispose();
+            }
+            dispose = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
